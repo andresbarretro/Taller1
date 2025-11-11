5e9b5e4e70ae013c406a40e1372bec2716f4604a
andresbarretro <andres_bandres@outlook.com>
2025-11-10 21:49:50 -0500

    solucion en clase del ejercicio10

---
+++ ejercicio11.js
@@ -0,0 +1,11 @@
+// Completa el siguiente programa e imprime la cantidad de unos (1) que haya en el arreglo:
+
+// const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];
+
+// El resultado debería ser el siguiente:
+
+// 11
+
+
+const nums = [ 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1 ];
+
